61d3d98b4d798f6e1cbdf356456adba416354929
pramodC135 <pramodmadhushan4@gmail.com>
2021-03-22 02:21:54 +0530

    insertItems

--- Items Management/src/com/Item.java
+++ Items Management/src/com/Item.java
@@ -1,6 +1,7 @@
 package com;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 
 public class Item {
 	
@@ -22,6 +23,44 @@ public class Item {
 			}
 			
 			return con;
+	}
+	
+	public String insertItem(String code, String name, String price, String desc)
+	{
+		String output = "";
+		
+		try
+		{
+			Connection con = connect();
+			if (con==null)
+			{
+				return "Error while connecting to the database";
+			}
+			
+			// Create a prepared statement
+			String query = "insert into items('itemID','itemCode','itemName','itemPrice',)"+" values(?, ?, ?, ?, ?)";
+			PreparedStatement preparedStmt = con.prepareStatement(query);
+			
+			//blinding values
+			preparedStmt.setInt(1, 0);
+			preparedStmt.setString(2, code);
+			preparedStmt.setString(3, name);
+			preparedStmt.setDouble(4, Double.parseDouble(price));
+			preparedStmt.setString(5, desc);
+			
+			//execute the statement
+			preparedStmt.execute();
+			con.close();
+			
+			output = "Insert Successfully";
+		}
+		catch(Exception e)
+		{
+			output = "Error while inserting";
+			System.err.println(e.getMessage());
+		}
+		
+		return output;
 	}
 
 
